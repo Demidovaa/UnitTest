1fbf8b6636552fb7e9d8cc1a6be98c2aab1ec410
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-03-28 19:44:40 +0300

    Work with defaultdict (module collections)

--- textdecoding/textstatistics.py
+++ textdecoding/textstatistics.py
@@ -4,10 +4,15 @@ Module for text statistics calculation.
 '''
 
 import sys
-from collections import Counter
+# from collections import Counter
+from collections import defaultdict
 
 def get_char_frequencies(text):
-    result = Counter(text)
+    # result = Counter(text)
+    result = defaultdict(int)
+    for symbol in text:
+        result[symbol] += 1
+
     return result
 
 def main():
