ef9eec66746b4b6ab0cecf9c1fa43ddc2b45158e
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-03-28 20:30:35 +0300

    Reformat Code

--- textdecoding/test/textstatistics_test.py
+++ textdecoding/test/textstatistics_test.py
@@ -7,7 +7,7 @@ import unittest
 import os
 import sys
 
-root_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_path)
 
 import textstatistics
@@ -30,29 +30,31 @@ class Test(unittest.TestCase):
         text = u'Не слушайте тех, кто говорит дурно о других и хорошо о вас.'
         result = textstatistics.get_char_frequencies(text)
         expected = {
-                    u'Н': 1, 
-                    u'е': 3, 
-                    u' ': 11, 
-                    u'с': 2, 
-                    u'л': 1, 
-                    u'у': 3, 
-                    u'ш': 2, 
-                    u'а': 2, 
-                    u'й': 1, 
-                    u'т': 4, 
-                    u'х': 3, 
-                    u',': 1, 
-                    u'к': 1, 
-                    u'о': 9,
-                    u'г': 2, 
-                    u'в': 2, 
-                    u'р': 4, 
-                    u'и': 3, 
-                    u'д': 2, 
-                    u'н': 1, 
-                    u'.': 1,                      
-                    }
+            u'Н': 1,
+            u'е': 3,
+            u' ': 11,
+            u'с': 2,
+            u'л': 1,
+            u'у': 3,
+            u'ш': 2,
+            u'а': 2,
+            u'й': 1,
+            u'т': 4,
+            u'х': 3,
+            u',': 1,
+            u'к': 1,
+            u'о': 9,
+            u'г': 2,
+            u'в': 2,
+            u'р': 4,
+            u'и': 3,
+            u'д': 2,
+            u'н': 1,
+            u'.': 1,
+        }
         self.assertDictEqual(result, expected)
+
+    # New UnitTest
 
     def test_get_char_frequencies_uniform_char(self):
         text = u'uniform'
@@ -64,22 +66,22 @@ class Test(unittest.TestCase):
         text = u'This is not a bug this is a feature'
         result = textstatistics.get_char_frequencies(text)
         expected = {
-                    u'T': 1,
-                    u'h': 2,
-                    u' ': 8,
-                    u'i': 4,
-                    u's': 4,
-                    u'n': 1,
-                    u'o': 1,
-                    u't': 3,
-                    u'a': 3,
-                    u'b': 1,
-                    u'u': 2,
-                    u'g': 1,
-                    u'f': 1,
-                    u'e': 2,
-                    u'r': 1,
-                    }
+            u'T': 1,
+            u'h': 2,
+            u' ': 8,
+            u'i': 4,
+            u's': 4,
+            u'n': 1,
+            u'o': 1,
+            u't': 3,
+            u'a': 3,
+            u'b': 1,
+            u'u': 2,
+            u'g': 1,
+            u'f': 1,
+            u'e': 2,
+            u'r': 1,
+        }
         self.assertDictEqual(result, expected)
 
 
